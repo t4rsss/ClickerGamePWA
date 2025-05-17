8d63d45b1ce0ae1c4b9749fedbcd257cdd669169
tarso <154676177+t4rsss@users.noreply.github.com>
2025-05-17 19:56:54 -0300

    Add files via upload

--- controller/gameController.js
+++ controller/gameController.js
@@ -232,43 +232,44 @@ function atualizarLoja() {
   }
 
     document.querySelectorAll(".upgrade-btn").forEach(btn => {
-    btn.addEventListener("click", async () => {
-        const categoria = btn.dataset.categoria;
-        const index = parseInt(btn.dataset.index);
-        const upgrade = upgrades[categoria][index];
+  btn.addEventListener("click", async () => {
+    const categoria = btn.dataset.categoria;
+    const index = parseInt(btn.dataset.index);
+    const upgrade = upgrades[categoria][index];
 
-        if (btc >= upgrade.preco) {
-        btc -= upgrade.preco;
-        upgrade.efeito();
+    if (btc >= upgrade.preco) {
+      btc -= upgrade.preco;
 
-        if (upgrade.repetivel) {
-            upgrade.nivel++;
-            upgrade.efeito();
-            upgrade.preco = Math.floor(upgrade.preco * 1.5);
-        } else {
-            upgrade.comprado = true;
-        }
+      if (upgrade.repetivel) {
+        upgrade.nivel++;
+        upgrade.preco = Math.floor(upgrade.preco * 1.5);
+      } else {
+        upgrade.comprado = true;
+      }
 
-        atualizarLoja();
-        atualizarDisplay();
-        salvarProgresso();
+      upgrade.efeito(); 
 
-        const user = auth.currentUser;
-        if (user) {
-            const uid = user.uid;
-            const compraRef = push(ref(db, `compras/${uid}`));
-            await set(compraRef, {
-            nomeUpgrade: upgrade.nome,
-            valor: upgrade.preco,
-            dataCompra: new Date().toISOString()
-            });
-        }
+      atualizarLoja();
+      atualizarDisplay();
+      salvarProgresso();
 
-        buysound.currentTime = 0;
-        buysound.play();
-        }
-    });
-    });
+      const user = auth.currentUser;
+      if (user) {
+        const uid = user.uid;
+        const compraRef = push(ref(db, `compras/${uid}`));
+        await set(compraRef, {
+          nomeUpgrade: upgrade.nome,
+          valor: upgrade.preco,
+          dataCompra: new Date().toISOString()
+        });
+      }
+
+      buysound.currentTime = 0;
+      buysound.play();
+    }
+  });
+});
+
 }
 
 function inicializarJogo() {
