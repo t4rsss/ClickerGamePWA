2761801537ee84727cfd52b171366ce85c6b8cf3
tarso <154676177+t4rsss@users.noreply.github.com>
2025-05-17 16:44:07 -0300

    Add files via upload

--- controller/gameController.js
+++ controller/gameController.js
@@ -1,16 +1,32 @@
-// Firebase imports
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import {
   getDatabase,
   ref,
-  update,
-  get,
   set,
+  get,
+  update,
   push
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const db = getDatabase();
-const auth = getAuth();
+// Inicialização obrigatória
+const firebaseConfig = {
+  apiKey: "AIzaSyBbd9rQZ1LwwTrMWY30txNugYsF_KVooGo",
+  authDomain: "prj-crypto-clicker.firebaseapp.com",
+  databaseURL: "https://prj-crypto-clicker-default-rtdb.firebaseio.com",
+  projectId: "prj-crypto-clicker",
+  storageBucket: "prj-crypto-clicker.appspot.com",
+  messagingSenderId: "1031485029809",
+  appId: "1:1031485029809:web:16eb4e1f22f17a524f1190",
+  measurementId: "G-5YM5FDLJF7"
+};
+
+const app = initializeApp(firebaseConfig); 
+const db = getDatabase(app);
+const auth = getAuth(app);
+
 
 // Sons
 const btnSound = new Audio('../assets/sounds/btnsound.mp3');
